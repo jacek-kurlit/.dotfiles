d65f85dd65ad256547bf23209c20940b965ac9ad
Jacek Kurlit <jacek.kurlit@gmail.com>
2023-03-26 21:38:22 +0200

    fold first try

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -18,12 +18,12 @@ return {
             added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
             modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
             deleted = "✖", -- this can only be used in the git_status source
-            renamed = "", -- this can only be used in the git_status source
+            renamed = "➜",
             -- Status type
-            untracked = "",
-            ignored = "",
-            unstaged = "",
-            staged = "",
+            untracked = "★",
+            ignored = "◌",
+            unstaged = "✗",
+            staged = "✓",
             conflict = "",
           },
         },
@@ -37,4 +37,21 @@ return {
       { "<leader>um", "<cmd>Glow<cr>", desc = "Preview markdown" },
     },
   },
+  -- FIXME: setup is not working I dont see mapping zR with description that I have probided
+  -- also folds are not created by default
+  -- {
+  --   "kevinhwang91/nvim-ufo",
+  --   dependencies = { "kevinhwang91/promise-async" },
+  --   keys = {
+  --     { "zR", "<cmd>lua require('ufo').openAllFolds()<cr>", desc = "Open all foldsxx" },
+  --     { "zM", "<cmd>lua require('ufo').closeAllFolds()<cr>", desc = "Close all folds" },
+  --   },
+  --   config = function()
+  --     require("ufo").setup({
+  --       provider_selector = function(_, _, _)
+  --         return { "treesitter", "indent" }
+  --       end,
+  --     })
+  --   end,
+  -- },
 }
