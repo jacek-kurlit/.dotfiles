547cdc4b5ad51fbc27be53b35f89be1eaa725f21
Jacek Kurlit <jacek.kurlit@gmail.com>
2023-03-26 22:07:40 +0200

    lightbulb for lsp saga

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -3,9 +3,6 @@ return {
     "glepnir/lspsaga.nvim",
     event = "BufRead",
     opts = {
-      lightbulb = {
-        enable = false,
-      },
       callhierarchy = {
         show_detail = true,
       },
