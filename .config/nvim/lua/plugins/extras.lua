e82573bc40b371194e583ff216bc09886bc09aac
Jacek Kurlit <jacek.kurlit@gmail.com>
2023-08-16 11:11:50 +0200

    bash added

--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -2,5 +2,6 @@ return {
   { import = "plugins.extras.lang.python" },
   { import = "plugins.extras.lang.nodejs" },
   { import = "plugins.extras.lang.svelte" },
+  { import = "plugins.extras.lang.bash" },
   { import = "plugins.extras.ai.import_all" },
 }
