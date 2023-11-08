caf5aec11ed90340ca35bae7ae1eaafbda5bc24b
Jacek Kurlit <jacek.kurlit@gmail.com>
2023-11-08 21:18:20 +0100

    added novide setup

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -14,3 +14,26 @@ vim.opt.formatoptions = "jqlnt"
 
 -- GPT env
 vim.env.OPENAI_API_HOST = "api.openai.com"
+
+-- neovide
+if vim.g.neovide then
+  -- padding
+  vim.g.neovide_padding_top = 0
+  vim.g.neovide_padding_bottom = 0
+  vim.g.neovide_padding_right = 0
+  vim.g.neovide_padding_left = 0
+
+  vim.g.neovide_hide_mouse_when_typing = true
+
+  -- scale
+  vim.g.neovide_scale_factor = 0.70
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.05)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.05)
+  end)
+end
