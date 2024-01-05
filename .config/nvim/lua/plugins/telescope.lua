eee9d9e972b11a89075a3e4c9e8de1c5be548722
Jacek Kurlit <jacek.kurlit@kitopi.com>
2024-01-05 15:54:43 +0100

    removed custom config for projects

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,21 +10,6 @@ return {
         end,
       },
       {
-        "ahmedkhalf/project.nvim",
-        opts = {
-          manual_mode = true,
-          -- Methods of detecting the root directory. **"lsp"** uses the native neovim
-          -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
-          -- order matters: if one is not detected, the other is used as fallback. You
-          -- can also delete or rearangne the detection methods.
-          detection_methods = { "pattern", "lsp" },
-          patterns = { ".git", ".svn", "Makefile", "package.json", "Cargo.toml" },
-          -- Don't calculate root dir on specific directories
-          -- Ex: { "~/.cargo/*", ... }
-          exclude_dirs = { "~/.cargo/*", "~/.rustup/*", "~/.local/share/nvim/site/pack/packer/*" },
-        },
-      },
-      {
         "debugloop/telescope-undo.nvim",
         event = "VeryLazy",
         config = function()
